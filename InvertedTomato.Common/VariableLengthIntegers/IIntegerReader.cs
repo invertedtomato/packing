152e282b5fafb6ae7d0176d59b2c2b31b140410b
Ben Thompson <ben@invertedtomato.com>
2016-08-17 07:28:30 +1000

    Added proposed new interfaces.

---
+++ InvertedTomato.Common/VariableLengthIntegers/IIntegerReader.cs
@@ -0,0 +1,8 @@
+﻿namespace InvertedTomato.VariableLengthIntegers {
+    public interface IIntegerReader {
+        bool TryRead(out ulong value);
+        ulong Read();
+
+        // static IEnumerable<ulong> Read(/* options */, byte[] input); 
+    }
+}
---
+++ InvertedTomato.Common/VariableLengthIntegers/IIntegerWriter.cs
@@ -0,0 +1,8 @@
+﻿namespace InvertedTomato.VariableLengthIntegers {
+    public interface IIntegerWriter {
+        void Write(ulong value);
+
+        // static byte[] Write (params ulong value);
+        // static byte[] Write (IEnumerable<ulong> values);
+    }
+}
