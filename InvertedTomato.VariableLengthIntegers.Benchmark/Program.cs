081baf8f11d9c64f1130fc33cfba3f78d472e8a5
Ben Thompson <ben@invertedtomato.com>
2016-08-17 14:44:54 +1000

    Added benchmark tool.

---
+++ InvertedTomato.VariableLengthIntegers.Benchmark/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace InvertedTomato.VariableLengthIntegers.Benchmark {
+    class Program {
+        static void Main(string[] args) {
+            using (var writer = new StreamWriter("output.csv", false)) {
+                writer.WriteLine("value,vlq,elias-omega");
+                for (ulong i = 1; i < 100000; i++) {
+                    writer.Write(i);
+                    writer.Write(",");
+                    writer.Write(VLQUnsignedWriter.CalculateBitLength(1, i));
+                    writer.Write(",");
+                    writer.Write(EliasOmegaUnsignedWriter.CalculateBitLength(false, i));
+                    writer.WriteLine();
+                }
+            }
+        }
+    }
+}
