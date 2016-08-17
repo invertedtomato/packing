ae54992b0b45a6353b31515bf45777fb691a8983
Ben Thompson <ben@invertedtomato.com>
2016-08-17 17:27:40 +1000

    Moved used bit counter into separate class.

---
+++ InvertedTomato.VariableLengthIntegers/Bits.cs
@@ -0,0 +1,14 @@
+﻿namespace InvertedTomato.VariableLengthIntegers {
+    public static class Bits {
+        public static byte CountUsed(ulong value) {
+            byte bits = 0;
+
+            do {
+                bits++;
+                value >>= 1;
+            } while (value > 0);
+
+            return bits;
+        }
+    }
+}
