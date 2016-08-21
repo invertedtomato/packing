5924b10db28d4663319efa74be601f1e0b7efa95
Ben Thompson <ben@invertedtomato.com>
2016-08-22 09:06:04 +1000

    Improved benchmark output.

--- InvertedTomato.IntegerCompression.Benchmark/Program.cs
+++ InvertedTomato.IntegerCompression.Benchmark/Program.cs
@@ -5,23 +5,23 @@ namespace InvertedTomato.IntegerCompression.Benchmark {
     class Program {
         static void Main(string[] args) {
             using (var writer = new StreamWriter("output.csv", false)) {
-                writer.WriteLine("value,min,vlq(1),vlq(2),vlq(3),vlq(4),elias-omega,elias-gamma");
+                writer.WriteLine("value,min,vlq,elias-omega,elias-gamma,tweedale,thompson(32),thompson(64)");
                 for (ulong i = 1; i < ulong.MaxValue / 10; i *= 2) {
                     writer.Write(i);
                     writer.Write(",");
-                    writer.Write((float)Bits.CountUsed(i) / (float)8);
+                    writer.Write(Bits.CountUsed(i));
                     writer.Write(",");
-                    writer.Write((float)VLQUnsignedWriter.CalculateBitLength(1, i) / (float)8);
+                    writer.Write(VLQUnsignedWriter.CalculateBitLength(0, i));
                     writer.Write(",");
-                    writer.Write((float)VLQUnsignedWriter.CalculateBitLength(2, i) / (float)8);
+                    writer.Write(EliasOmegaUnsignedWriter.CalculateBitLength(false, i));
                     writer.Write(",");
-                    writer.Write((float)VLQUnsignedWriter.CalculateBitLength(3, i) / (float)8);
+                    writer.Write(GuessEliasGammaLength(i));
                     writer.Write(",");
-                    writer.Write((float)VLQUnsignedWriter.CalculateBitLength(4, i) / (float)8);
+                    writer.Write(GuessTweedaleLength(i));
                     writer.Write(",");
-                    writer.Write((float)EliasOmegaUnsignedWriter.CalculateBitLength(false, i) / (float)8);
+                    writer.Write(GuessThompsonLength32(i));
                     writer.Write(",");
-                    writer.Write((float)GuessEliasGammaLength(i) / (float)8);
+                    writer.Write(GuessThompsonLength64(i));
                     writer.WriteLine();
                 }
             }
@@ -29,6 +29,49 @@ namespace InvertedTomato.IntegerCompression.Benchmark {
 
         private static int GuessEliasGammaLength(ulong value) {
             return Bits.CountUsed(value) * 2 - 1;
+        }
+
+        private static int GuessTweedaleLength(ulong value) {
+            var headerBits = 6;
+            var valueBits = Bits.CountUsed(value);
+
+            return headerBits + valueBits;
+        }
+
+        private static int GuessThompsonLength32(ulong v) {
+            var headerBits = 5;
+
+            if(v > uint.MaxValue) {
+                return 0;
+            }
+            var l = (ulong)Bits.CountUsed(v) - 1; // No need for the MSB
+
+            var valueBits = Bits.CountUsed(v) - 1; // No need for the MSB
+
+            return headerBits + valueBits;
+
+            /* ENCODE:
+             * Let v be the value to be encoded (max 64 bits)
+             * Let l be the number of bits in v, minus 1
+             * Write l as 6-bits
+             * Write v except the MSB
+             */
+        }
+        private static int GuessThompsonLength64(ulong v) {
+            var headerBits = 6;
+
+            var l = (ulong)Bits.CountUsed(v) - 1; // No need for the MSB
+
+            var valueBits = Bits.CountUsed(v)-1 ; // No need for the MSB
+
+            return headerBits + valueBits ;
+
+            /* ENCODE:
+             * Let v be the value to be encoded (max 64 bits)
+             * Let l be the number of bits in v, minus 1
+             * Write l as 6-bits
+             * Write v except the MSB
+             */
         }
     }
 }
