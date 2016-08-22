fe12cbbdf32cb6905005a9d9651507481e968a9d
Ben Thompson <ben@invertedtomato.com>
2016-08-22 12:51:58 +1000

    Fixed Thompson-Alpha tests.

--- InvertedTomato.IntegerCompression.Benchmark/Program.cs
+++ InvertedTomato.IntegerCompression.Benchmark/Program.cs
@@ -6,7 +6,7 @@ namespace InvertedTomato.IntegerCompression.Benchmark {
     class Program {
         static void Main(string[] args) {
             using (var writer = new StreamWriter("output.csv", false)) {
-                writer.WriteLine("value,min,vlq,elias-omega,elias-gamma,tweedale,thompson(32),thompson(64)");
+                writer.WriteLine("value,min,vlq,elias-omega,elias-gamma,thompson-alpha(6)");
                 for (ulong i = 1; i < ulong.MaxValue / 10; i *= 2) {
                     writer.Write(i);
                     writer.Write(",");
@@ -14,65 +14,14 @@ namespace InvertedTomato.IntegerCompression.Benchmark {
                     writer.Write(",");
                     writer.Write(VLQUnsignedWriter.CalculateBitLength(0, i));
                     writer.Write(",");
-                    writer.Write(EliasOmegaUnsignedWriter.CalculateBitLength( i));
+                    writer.Write(EliasOmegaUnsignedWriter.CalculateBitLength(i));
                     writer.Write(",");
-                    writer.Write(GuessEliasGammaLength(i));
+                    writer.Write(EliasGammaUnsignedWriter.CalculateBitLength(i));
                     writer.Write(",");
-                    writer.Write(GuessTweedaleLength(i));
-                    writer.Write(",");
-                    writer.Write(GuessThompsonLength32(i));
-                    writer.Write(",");
-                    writer.Write(GuessThompsonLength64(i));
+                    writer.Write(ThompsonAlphaUnsignedWriter.CalculateBitLength(6, i));
                     writer.WriteLine();
                 }
             }
-        }
-
-        private static int GuessEliasGammaLength(ulong value) {
-            return Bits.CountUsed(value) * 2 - 1;
-        }
-
-        private static int GuessTweedaleLength(ulong value) {
-            var headerBits = 6;
-            var valueBits = Bits.CountUsed(value);
-
-            return headerBits + valueBits;
-        }
-
-        private static int GuessThompsonLength32(ulong v) {
-            var headerBits = 5;
-
-            if(v > uint.MaxValue) {
-                return 0;
-            }
-            var l = (ulong)Bits.CountUsed(v) - 1; // No need for the MSB
-
-            var valueBits = Bits.CountUsed(v) - 1; // No need for the MSB
-
-            return headerBits + valueBits;
-
-            /* ENCODE:
-             * Let v be the value to be encoded (max 64 bits)
-             * Let l be the number of bits in v, minus 1
-             * Write l as 6-bits
-             * Write v except the MSB
-             */
-        }
-        private static int GuessThompsonLength64(ulong v) {
-            var headerBits = 6;
-
-            var l = (ulong)Bits.CountUsed(v) - 1; // No need for the MSB
-
-            var valueBits = Bits.CountUsed(v)-1 ; // No need for the MSB
-
-            return headerBits + valueBits ;
-
-            /* ENCODE:
-             * Let v be the value to be encoded (max 64 bits)
-             * Let l be the number of bits in v, minus 1
-             * Write l as 6-bits
-             * Write v except the MSB
-             */
         }
     }
 }
--- InvertedTomato.IntegerCompression.Tests/ThompsonAlphaUnsignedReaderTests.cs
+++ InvertedTomato.IntegerCompression.Tests/ThompsonAlphaUnsignedReaderTests.cs
@@ -6,82 +6,82 @@ using System.Linq;
 namespace InvertedTomato.IntegerCompression.Tests {
     [TestClass]
     public class ThompsonAlphaUnsignedReaderTests {
-        private ulong TestRead(string input) {
+        private ulong Read(string input) {
             return ThompsonAlphaUnsignedReader.ReadAll(ByteArrayUtility.ParseBinaryString(input)).First();
         }
 
         [TestMethod]
         public void Read_0() {
-            Assert.AreEqual((ulong)0, TestRead("00000000"));
+            Assert.AreEqual((ulong)0, Read("00000000"));
         }
         [TestMethod]
         public void Read_1() {
-            Assert.AreEqual((ulong)1, TestRead("00000100"));
+            Assert.AreEqual((ulong)1, Read("00000100"));
         }
         [TestMethod]
         public void Read_2() {
-            Assert.AreEqual((ulong)2, TestRead("00000110"));
+            Assert.AreEqual((ulong)2, Read("00000110"));
         }
         [TestMethod]
         public void Read_3() {
-            Assert.AreEqual((ulong)3, TestRead("00001000"));
+            Assert.AreEqual((ulong)3, Read("00001000"));
         }
         [TestMethod]
         public void Read_4() {
-            Assert.AreEqual((ulong)4, TestRead("00001001"));
+            Assert.AreEqual((ulong)4, Read("00001001"));
         }
         [TestMethod]
         public void Read_5() {
-            Assert.AreEqual((ulong)5, TestRead("00001010"));
+            Assert.AreEqual((ulong)5, Read("00001010"));
         }
         [TestMethod]
         public void Read_6() {
-            Assert.AreEqual((ulong)6, TestRead("00001011"));
+            Assert.AreEqual((ulong)6, Read("00001011"));
         }
         [TestMethod]
         public void Read_7() {
-            Assert.AreEqual((ulong)7, TestRead("00001100 00000000"));
+            Assert.AreEqual((ulong)7, Read("00001100 00000000"));
         }
         [TestMethod]
         public void Read_8() {
-            Assert.AreEqual((ulong)8, TestRead("00001100 10000000"));
+            Assert.AreEqual((ulong)8, Read("00001100 10000000"));
         }
         [TestMethod]
         public void Read_9() {
-            Assert.AreEqual((ulong)9, TestRead("00001101 00000000"));
+            Assert.AreEqual((ulong)9, Read("00001101 00000000"));
         }
         [TestMethod]
         public void Read_10() {
-            Assert.AreEqual((ulong)10, TestRead("00001101 10000000"));
+            Assert.AreEqual((ulong)10, Read("00001101 10000000"));
         }
         [TestMethod]
         public void Read_11() {
-            Assert.AreEqual((ulong)11, TestRead("00001110 00000000"));
+            Assert.AreEqual((ulong)11, Read("00001110 00000000"));
         }
         [TestMethod]
         public void Read_12() {
-            Assert.AreEqual((ulong)12, TestRead("00001110 10000000"));
+            Assert.AreEqual((ulong)12, Read("00001110 10000000"));
         }
         [TestMethod]
         public void Read_13() {
-            Assert.AreEqual((ulong)13, TestRead("00001111 00000000"));
+            Assert.AreEqual((ulong)13, Read("00001111 00000000"));
         }
         [TestMethod]
         public void Read_14() {
-            Assert.AreEqual((ulong)14, TestRead("00001111 10000000"));
+            Assert.AreEqual((ulong)14, Read("00001111 10000000"));
         }
         [TestMethod]
         public void Read_15() {
-            Assert.AreEqual((ulong)15, TestRead("00010000 00000000"));
+            Assert.AreEqual((ulong)15, Read("00010000 00000000"));
         }
         [TestMethod]
         public void Read_16() {
-            Assert.AreEqual((ulong)16, TestRead("00010000 01000000"));
+            Assert.AreEqual((ulong)16, Read("00010000 01000000"));
         }
 
         [TestMethod]
         public void Read_1_1_1() {
-            var result = ThompsonAlphaUnsignedReader.ReadAll(ByteArrayUtility.ParseBinaryString("00000110 00001100 00011000"));
+            var result = ThompsonAlphaUnsignedReader.ReadAll(ByteArrayUtility.ParseBinaryString("00000100 00001000 00010000"));
             Assert.AreEqual(3, result.Count());
             Assert.AreEqual((ulong)1, result.ElementAt(0));
             Assert.AreEqual((ulong)1, result.ElementAt(1));
@@ -89,7 +89,7 @@ namespace InvertedTomato.IntegerCompression.Tests {
         }
         [TestMethod]
         public void Read_4_4_4() {
-            var result = ThompsonAlphaUnsignedReader.ReadAll(ByteArrayUtility.ParseBinaryString("00001110 00000111 00000011 10000001 11000000"));
+            var result = ThompsonAlphaUnsignedReader.ReadAll(ByteArrayUtility.ParseBinaryString("00001001 00001001 00001001"));
             Assert.AreEqual(3, result.Count());
             Assert.AreEqual((ulong)4, result.ElementAt(0));
             Assert.AreEqual((ulong)4, result.ElementAt(1));
