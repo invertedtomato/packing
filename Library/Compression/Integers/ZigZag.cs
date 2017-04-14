4f39449c21aa77d6445ad1edd4a0fef53fd4c1bc
Ben Thompson <ben@invertedtomato.com>
2017-04-14 15:03:47 +1000

    Added comments.

--- Library/Compression/Integers/ZigZag.cs
+++ Library/Compression/Integers/ZigZag.cs
@@ -3,10 +3,20 @@
     /// Encode signed values as unsigned using ProtoBuffer ZigZag bijection encoding algorithm. https://developers.google.com/protocol-buffers/docs/encoding
     /// </summary>
     public static class ZigZag {
+        /// <summary>
+        /// Encode a signed long into an ZigZag unsigned long.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static ulong Encode(long value) {
             return (ulong)((value << 1) ^ (value >> 63));
         }
 
+        /// <summary>
+        /// Decode a ZigZag unsigned long back into a signed long. 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static long Decode(ulong value) {
             var casted = (long)value;
             return (casted >> 1) ^ (-(casted & 1));
