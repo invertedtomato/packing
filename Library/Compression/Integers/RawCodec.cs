8afcea4d62593b5e9ce282b5c0c21b880e9a5709
Ben Thompson <ben@invertedtomato.com>
2017-04-17 18:13:44 +1000

    Added raw codec for the sake of comparison. (This codec doesn't compress)

---
+++ Library/Compression/Integers/RawCodec.cs
@@ -0,0 +1,120 @@
+﻿using InvertedTomato.IO.Buffers;
+using System;
+
+namespace InvertedTomato.Compression.Integers {
+    public class RawCodec : IIntegerCodec {
+        public byte[] Compress(long[] symbols) {
+#if DEBUG
+            if (null == symbols) {
+                throw new ArgumentNullException("symbols");
+            }
+#endif
+
+            // Prepare input buffer
+            var input = new Buffer<ulong>(symbols.Length);
+
+            // Perform signed conversion
+            foreach (var i in symbols) {
+                input.Enqueue(ZigZag.Encode(i));
+            }
+
+            // Compress
+            var output = new Buffer<byte>(input.Used * 2);
+            while (!Compress(input, output)) {
+                output = output.Resize(output.MaxCapacity * 2);
+            }
+
+            return output.ToArray();
+        }
+
+        public bool Compress(Buffer<ulong> input, Buffer<byte> output) {
+#if DEBUG
+            if (null == input) {
+                throw new ArgumentNullException("input");
+            }
+            if (null == output) {
+                throw new ArgumentNullException("output");
+            }
+#endif
+
+            // Loop while there is still input
+            while (!input.IsEmpty) {
+                // Abort if there isn't enough free space
+                if (output.Available < 8) {
+                    return false; // INPUT isn't empty
+                }
+
+                // Convert symbol to bytes
+                var raw = BitConverter.GetBytes(input.Dequeue());
+
+                // Write to output
+                output.EnqueueArray(raw);
+            }
+
+            // Return
+            return true; // INPUT is empty
+        }
+
+        public long[] Decompress(byte[] raw) {
+#if DEBUG
+            if (null == raw) {
+                throw new ArgumentNullException("raw");
+            }
+#endif
+
+            // Prepare buffers
+            var input = new Buffer<byte>(raw);
+            var output = new Buffer<ulong>(raw.Length);
+
+            // Decompress
+            while (!Decompress(input, output)) {
+                output = output.Resize(output.MaxCapacity * 2);
+            }
+
+            // Perform signed conversion
+            var symbols = new long[output.Used];
+            for (var i = 0; i < symbols.Length; i++) {
+                symbols[i] = ZigZag.Decode(output.Dequeue());
+            }
+
+            return symbols;
+        }
+
+        public bool Decompress(Buffer<byte> input, Buffer<ulong> output) {
+#if DEBUG
+            if (null == input) {
+                throw new ArgumentNullException("input");
+            }
+            if (null == output) {
+                throw new ArgumentNullException("output");
+            }
+#endif
+
+            // Iterate through input
+            while (input.Used >= 8) {
+                // Check there is enough output space
+                if (output.IsFull) {
+                    return false;
+                }
+
+                // Get symbol
+                var symbol = BitConverter.ToUInt64(input.GetUnderlying(), input.Start); // Low-level cheat to save allocating unnesscessary memory
+                input.MoveStart(+8);
+
+                // Add to output
+                output.Enqueue(symbol);
+            }
+
+#if DEBUG
+            if (input.Used > 0 && input.Used < 8) {
+                throw new FormatException("Input ends with a partial symbol - bytes are missing or the input is corrupt.");
+            }
+#endif
+
+            // Return
+            return true;
+        }
+
+        public static readonly ulong MaxValue = ulong.MaxValue;
+    }
+}
---
+++ Tests/Compression/Integers/RawCodecTests.cs
@@ -0,0 +1,163 @@
+﻿using InvertedTomato.IO.Buffers;
+using InvertedTomato.IO.Bits;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertedTomato.Compression.Integers {
+    [TestClass]
+    public class RawCodecTests {
+        public string CompressMany(ulong[] set, int outputBufferSize = 8) {
+            var input = new Buffer<ulong>(set);
+            var output = new Buffer<byte>(outputBufferSize);
+            var codec = new RawCodec();
+            Assert.IsTrue(codec.Compress(input, output));
+
+            return output.ToArray().ToBinaryString();
+        }
+        public string CompressOne(ulong value, int outputBufferSize = 8) {
+            return CompressMany(new ulong[] { value }, outputBufferSize);
+        }
+
+        [TestMethod]
+        public void Compress_Empty() {
+            Assert.AreEqual("", CompressMany(new ulong[] { }));
+        }
+        [TestMethod]
+        public void Compress_0() {
+            Assert.AreEqual("00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000", CompressOne(0));
+        }
+        [TestMethod]
+        public void Compress_1() {
+            Assert.AreEqual("00000001 00000000 00000000 00000000 00000000 00000000 00000000 00000000", CompressOne(1));
+        }
+        [TestMethod]
+        public void Compress_2() {
+            Assert.AreEqual("00000010 00000000 00000000 00000000 00000000 00000000 00000000 00000000", CompressOne(2));
+        }
+        [TestMethod]
+        public void Compress_3() {
+            Assert.AreEqual("00000011 00000000 00000000 00000000 00000000 00000000 00000000 00000000", CompressOne(3));
+        }
+        [TestMethod]
+        public void Compress_Max() {
+            Assert.AreEqual("11111111 11111111 11111111 11111111 11111111 11111111 11111111 11111111", CompressOne(ulong.MaxValue));
+        }
+        [TestMethod]
+        public void Compress_1_1_1() {
+            Assert.AreEqual("00000001 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000001 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000001 00000000 00000000 00000000 00000000 00000000 00000000 00000000", CompressMany(new ulong[] { 1, 1, 1 }, 3 * 8));
+        }
+        [TestMethod]
+        public void Compress_OutputTooSmall() {
+            var input = new Buffer<ulong>(new ulong[] { 128 });
+            var output = new Buffer<byte>(1);
+            var codec = new RawCodec();
+            Assert.IsFalse(codec.Compress(input, output));
+            Assert.AreEqual("", output.ToArray().ToBinaryString());
+            Assert.AreEqual(0, input.Start);
+            Assert.AreEqual(1, input.End);
+        }
+
+
+
+        private ulong[] DecompressMany(string value, int count) {
+            var input = new Buffer<byte>(BitOperation.ParseToBytes(value));
+            var output = new Buffer<ulong>(count);
+            var codec = new RawCodec();
+            Assert.IsTrue(codec.Decompress(input, output));
+
+            return output.ToArray();
+        }
+        private ulong DecompressOne(string value) {
+            var set = DecompressMany(value, 1);
+            return set[0];
+        }
+
+        [TestMethod]
+        public void Decompress_Empty() {
+            Assert.AreEqual(0, DecompressMany("", 0).Length);
+        }
+        [TestMethod]
+        public void Decompress_0() {
+            Assert.AreEqual((ulong)0, DecompressOne("00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000"));
+        }
+        [TestMethod]
+        public void Decompress_1() {
+            Assert.AreEqual((ulong)1, DecompressOne("00000001 00000000 00000000 00000000 00000000 00000000 00000000 00000000"));
+        }
+        [TestMethod]
+        public void Decompress_2() {
+            Assert.AreEqual((ulong)2, DecompressOne("00000010 00000000 00000000 00000000 00000000 00000000 00000000 00000000"));
+        }
+        [TestMethod]
+        public void Decompress_3() {
+            Assert.AreEqual((ulong)3, DecompressOne("00000011 00000000 00000000 00000000 00000000 00000000 00000000 00000000"));
+        }
+        [TestMethod]
+        public void Decompress_Max() {
+            Assert.AreEqual(RawCodec.MaxValue, DecompressOne("11111111 11111111 11111111 11111111 11111111 11111111 11111111 11111111"));
+        }
+        [TestMethod]
+        public void Decompress_1_1_1() {
+            var set = DecompressMany("00000001 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000001 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000001 00000000 00000000 00000000 00000000 00000000 00000000 00000000", 3);
+            Assert.AreEqual(3, set.Length);
+            Assert.AreEqual((ulong)1, set[0]);
+            Assert.AreEqual((ulong)1, set[1]);
+            Assert.AreEqual((ulong)1, set[2]);
+        }
+        [TestMethod]
+        public void Decompress_OutputTooSmall() {
+            var input = new Buffer<byte>(BitOperation.ParseToBytes("00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000"));
+            var output = new Buffer<ulong>(1);
+            var codec = new RawCodec();
+            Assert.IsFalse(codec.Decompress(input, output));
+            Assert.AreEqual(1, output.Used);
+            Assert.AreEqual((ulong)0, output.Dequeue());
+        }
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void Decompress_InputClipped() {
+            var input = new Buffer<byte>(BitOperation.ParseToBytes("00000000"));
+            var output = new Buffer<ulong>(1);
+            var codec = new RawCodec();
+            codec.Decompress(input, output);
+        }
+
+
+
+        [TestMethod]
+        public void CompressDecompress_First1000_Series() {
+            for (ulong input = 0; input < 1000; input++) {
+                var encoded = CompressOne(input);
+                var output = DecompressOne(encoded);
+
+                Assert.AreEqual(input, output);
+            }
+        }
+        [TestMethod]
+        public void CompressDecompress_First1000_Parallel_Basic() {
+            var codec = new RawCodec();
+
+            // Create input
+            var input = new long[1000];
+            for (var i = 0; i < input.Length; i++) {
+                input[i] = i;
+            }
+
+            // Compress
+            var compressed = codec.Compress(input);
+
+            // Decompress
+            var output = codec.Decompress(compressed);
+
+            // Validate
+            Assert.AreEqual(1000, output.Length);
+            for (long i = 0; i < 1000; i++) {
+                Assert.AreEqual(i, output[i]);
+            }
+        }
+    }
+}
