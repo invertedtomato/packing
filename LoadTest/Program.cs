83554c5ee2633dd4d7a05056207b6e297fa26d47
Ben Thompson <ben@invertedtomato.com>
2018-02-02 07:49:45 +1000

    Fixed loadtest not working with new interface.

--- LoadTest/Program.cs
+++ LoadTest/Program.cs
@@ -1,5 +1,4 @@
 ﻿using InvertedTomato.Compression.Integers;
-using InvertedTomato.IO.Buffers;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -27,13 +26,16 @@ namespace InvertedTomato.Compression.Integer.LoadTest {
             // Compress: 10291ms 7.41MB/s
             // Decompress: 5819ms 13.11MB / s
 
+            // #5
+            // FIBONACCI
+            // Compress: 5986ms 12.75MB / s Total 38MB
+            // Decompress: 3455ms 22.08MB / s
+            // VLQ
+            // Compress: 386ms 197.65MB / s Total 36MB
+            // Decompress: 874ms 87.29MB / s
+
             Int32 min = 100000;
             Int32 count = 10000000;
-
-            //////////////////////////////////////////
-            Console.WriteLine("FIBONACCI");
-            var stopWatch = Stopwatch.StartNew();
-            Codec codec = new FibonacciCodec();
 
             // Seed
             var input = new List<UInt64>((Int32)count);
@@ -41,74 +43,66 @@ namespace InvertedTomato.Compression.Integer.LoadTest {
                 input.Add((UInt64)v);
             }
 
-            // Compress
+            //////////////////////////////////////////
+            Console.WriteLine("FIBONACCI");
+            Codec codec = new FibonacciCodec();
             var compressed = new MemoryStream((Int32)count * 5);
 
+            // Compress
+            var stopWatch = Stopwatch.StartNew();
+            codec.CompressUnsigned(compressed, input.ToArray());
             stopWatch.Stop();
             Console.WriteLine("Compress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((Double)count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s Total " + (compressed.Length / 1024 / 1024) + "MB");
-            stopWatch = Stopwatch.StartNew();
+
+            // Rewind
+            compressed.Position = 0;
 
             // Decompress
-            var output =  codec.DecompressUnsigned(compressed,input.Count).ToList();
-
-            // Validate
-            for (var v = min; v < min + count; v++) {
-                if ((int)output[v] != v) {
-                    throw new Exception("Incorrect result. Expected " + v + " got " + v + ".");
-                }
-            }
-
+            stopWatch = Stopwatch.StartNew();
+            var output = codec.DecompressUnsigned(compressed, input.Count).ToList();
             stopWatch.Stop();
             Console.WriteLine("Decompress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((Double)count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s");
 
-
-            //////////////////////////////////////////
-            /*
-            Console.WriteLine("VLQ");
-            stopWatch = Stopwatch.StartNew();
-            codec = new VLQCodec();
-
-            // Seed
-            input = new Buffer<ulong>((int)count);
-            for (ulong v = min; v < min + count; v++) {
-                input.Enqueue(v);
-            }
-
-            // Compress
-            compressed = new Stream((int)count * 5);
-            while (!codec.Compress(input, compressed)) {
-                Console.Write("Expanding compression buffer... ");
-                compressed = compressed.Resize(compressed.MaxCapacity * 2);
-                Console.WriteLine(compressed.MaxCapacity);
-            }
-
-            stopWatch.Stop();
-            Console.WriteLine("Compress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((double)count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s Total " + (compressed.Used / 1024 / 1024) + "MB");
-            stopWatch = Stopwatch.StartNew();
-
-            // Decompress
-            output = new Buffer<ulong>((int)count);
-            while (!codec.Decompress(compressed, output)) {
-                Console.Write("Expanding decompression buffer...");
-                output = output.Resize(output.MaxCapacity * 2);
-                Console.WriteLine(output.MaxCapacity);
-            }
-
             // Validate
-            for (ulong v = min; v < min + count; v++) {
-                if (output.Dequeue() != v) {
-                    throw new Exception("Incorrect result. Expected " + v + " got " + v + ".");
+            var pos = 0;
+            for (var v = min; v < min + count; v++) {
+                if ((int)output[pos++] != v) {
+                    throw new Exception("Incorrect result. Expected " + v + " got " + output[v] + ".");
                 }
             }
 
+
+            //////////////////////////////////////////
+            Console.WriteLine("VLQ");
+            codec = new VLQCodec();
+            compressed = new MemoryStream((Int32)count * 5);
+
+            // Compress
+            stopWatch = Stopwatch.StartNew();
+            codec.CompressUnsigned(compressed, input.ToArray());
             stopWatch.Stop();
-            Console.WriteLine("Decompress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((double)count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s");
+            Console.WriteLine("Compress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((Double)count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s Total " + (compressed.Length / 1024 / 1024) + "MB");
 
-    */
+            // Rewind
+            compressed.Position = 0;
+
+            // Decompress
+            stopWatch = Stopwatch.StartNew();
+            output = codec.DecompressUnsigned(compressed, input.Count).ToList();
+            stopWatch.Stop();
+            Console.WriteLine("Decompress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((Double)count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s");
+
+            // Validate
+            pos = 0;
+            for (var v = min; v < min + count; v++) {
+                if ((int)output[pos++] != v) {
+                    throw new Exception("Incorrect result. Expected " + v + " got " + output[v] + ".");
+                }
+            }
 
 
 
-            Console.WriteLine("Done.");
+            Console.WriteLine("\nDone.");
             Console.ReadKey(true);
         }
     }
