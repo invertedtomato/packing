b2e93329aa5de5077f4bd284c24f9c7f7c04b6d3
Ben Thompson <ben.thompson@lup.events>
2022-06-23 20:18:34 +1000

    Shook the dust of the project and welcomed it into 2022

--- LoadTest/Program.cs
+++ LoadTest/Program.cs
@@ -1,109 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//#1
+// Write: 7865ms 9.7MB/s
+// Read: 17392ms 4.39MB / s
+// ASync Read: 2935ms 25.99MB / s
+// #2
+// Write: 14,115ms 5.41MB/s
+// Read: 6,226ms 12.25MB/s
+
+// #3
+// Compress: 11801ms 6.47MB / s
+// Decompress: 6967ms 10.95MB / s
+
+// #4
+// Compress: 10291ms 7.41MB/s
+// Decompress: 5819ms 13.11MB / s
+
+// #5
+// FIBONACCI
+// Compress: 5986ms 12.75MB / s Total 38MB
+// Decompress: 3455ms 22.08MB / s
+// VLQ
+// Compress: 386ms 197.65MB / s Total 36MB
+// Decompress: 874ms 87.29MB / s
+
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using InvertedTomato.Compression.Integers;
 
-namespace InvertedTomato.Compression.Integer.LoadTest {
-	internal class Program {
-		private static void Main(String[] args) {
-			//#1
-			// Write: 7865ms 9.7MB/s
-			// Read: 17392ms 4.39MB / s
-			// ASync Read: 2935ms 25.99MB / s
+var min = 100000;
+var count = 10000000;
 
-			// #2
-			// Write: 14,115ms 5.41MB/s
-			// Read: 6,226ms 12.25MB/s
-
-			// #3
-			// Compress: 11801ms 6.47MB / s
-			// Decompress: 6967ms 10.95MB / s
-
-			// #4
-			// Compress: 10291ms 7.41MB/s
-			// Decompress: 5819ms 13.11MB / s
-
-			// #5
-			// FIBONACCI
-			// Compress: 5986ms 12.75MB / s Total 38MB
-			// Decompress: 3455ms 22.08MB / s
-			// VLQ
-			// Compress: 386ms 197.65MB / s Total 36MB
-			// Decompress: 874ms 87.29MB / s
-
-			var min = 100000;
-			var count = 10000000;
-
-			// Seed
-			var input = new List<UInt64>(count);
-			for (var v = min; v < min + count; v++) {
-				input.Add((UInt64) v);
-			}
-
-			//////////////////////////////////////////
-			Console.WriteLine("FIBONACCI");
-			Codec wave2Codec = new FibonacciCodec();
-			var compressed = new MemoryStream(count * 5);
-
-			// Compress
-			var stopWatch = Stopwatch.StartNew();
-			wave2Codec.EncodeMany(compressed, input.ToArray());
-			stopWatch.Stop();
-			Console.WriteLine("Compress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((Double) count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s Total " + compressed.Length / 1024 / 1024 + "MB");
-
-			// Rewind
-			compressed.Position = 0;
-
-			// Decompress
-			stopWatch = Stopwatch.StartNew();
-			var output = new UInt64[count];
-			wave2Codec.DecodeMany(compressed, output);
-			stopWatch.Stop();
-			Console.WriteLine("Decompress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((Double) count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s");
-
-			// Validate
-			var pos = 0;
-			for (var v = min; v < min + count; v++) {
-				if ((Int32) output[pos++] != v) {
-					throw new Exception("Incorrect result. Expected " + v + " got " + output[v] + ".");
-				}
-			}
-
-
-			//////////////////////////////////////////
-			Console.WriteLine("VLQ");
-			wave2Codec = new VlqCodec();
-			compressed = new MemoryStream(count * 5);
-
-			// Compress
-			stopWatch = Stopwatch.StartNew();
-			wave2Codec.EncodeMany(compressed, input.ToArray());
-			stopWatch.Stop();
-			Console.WriteLine("Compress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((Double) count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s Total " + compressed.Length / 1024 / 1024 + "MB");
-
-			// Rewind
-			compressed.Position = 0;
-
-			// Decompress
-			stopWatch = Stopwatch.StartNew();
-			output = new UInt64[count];
-			wave2Codec.DecodeMany(compressed, output);
-			stopWatch.Stop();
-			Console.WriteLine("Decompress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((Double) count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s");
-
-			// Validate
-			pos = 0;
-			for (var v = min; v < min + count; v++) {
-				if ((Int32) output[pos++] != v) {
-					throw new Exception("Incorrect result. Expected " + v + " got " + output[v] + ".");
-				}
-			}
-
-
-			Console.WriteLine("\nDone.");
-			Console.ReadKey(true);
-		}
-	}
+// Seed
+var input = new List<UInt64>(count);
+for (var v = min; v < min + count; v++)
+{
+    input.Add((UInt64) v);
 }
+
+//////////////////////////////////////////
+Console.WriteLine("FIBONACCI");
+Codec wave2Codec = new FibonacciCodec();
+var compressed = new MemoryStream(count * 5);
+
+// Compress
+var stopWatch = Stopwatch.StartNew();
+wave2Codec.EncodeMany(compressed, input.ToArray());
+stopWatch.Stop();
+Console.WriteLine("Compress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((Double) count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s Total " + compressed.Length / 1024 / 1024 + "MB");
+
+// Rewind
+compressed.Position = 0;
+
+// Decompress
+stopWatch = Stopwatch.StartNew();
+var output = new UInt64[count];
+wave2Codec.DecodeMany(compressed, output);
+stopWatch.Stop();
+Console.WriteLine("Decompress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((Double) count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s");
+
+// Validate
+var pos = 0;
+for (var v = min; v < min + count; v++)
+{
+    if ((Int32) output[pos++] != v)
+    {
+        throw new Exception("Incorrect result. Expected " + v + " got " + output[v] + ".");
+    }
+}
+
+
+//////////////////////////////////////////
+Console.WriteLine("VLQ");
+wave2Codec = new VlqCodec();
+compressed = new MemoryStream(count * 5);
+
+// Compress
+stopWatch = Stopwatch.StartNew();
+wave2Codec.EncodeMany(compressed, input.ToArray());
+stopWatch.Stop();
+Console.WriteLine("Compress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((Double) count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s Total " + compressed.Length / 1024 / 1024 + "MB");
+
+// Rewind
+compressed.Position = 0;
+
+// Decompress
+stopWatch = Stopwatch.StartNew();
+output = new UInt64[count];
+wave2Codec.DecodeMany(compressed, output);
+stopWatch.Stop();
+Console.WriteLine("Decompress: " + stopWatch.ElapsedMilliseconds + "ms " + Math.Round((Double) count * 1000 * 8 / 1024 / 1024 / stopWatch.ElapsedMilliseconds, 2) + "MB/s");
+
+// Validate
+pos = 0;
+for (var v = min; v < min + count; v++)
+{
+    if ((Int32) output[pos++] != v)
+    {
+        throw new ("Incorrect result. Expected " + v + " got " + output[v] + ".");
+    }
+}
+
+Console.WriteLine("\nDone.");
+Console.ReadKey(true);
--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using System.IO;
-using InvertedTomato.Compression.Integers;
+﻿using InvertedTomato.Compression.Integers;
 
-namespace InvertedTomato.Compression.Integers.Sample {
-	internal class Program {
-		private static void Main(String[] args) {
-			// Instantiate the codec ready to compress
-			Codec codec = new FibonacciCodec(); // Using "InvertedTomato.Compression.Integers.Wave3"
+// Instantiate the codec ready to compress
+Codec codec = new FibonacciCodec(); // Using "InvertedTomato.Compression.Integers.Wave3"
 
-			using (var stream = new MemoryStream()) {
-				// Compress data - 3x8 bytes = 24bytes uncompressed
-				codec.EncodeMany(stream, new UInt64[] {1, 2, 3});
-				Console.WriteLine("Compressed data is " + stream.Length + " bytes"); // Output: Compressed data is 2 bytes
+// Compress data - 3x8 bytes = 24bytes uncompressed
+using var stream = new MemoryStream();
+codec.EncodeMany(stream, new ulong[] {1, 2, 3,});
+Console.WriteLine("Compressed data is " + stream.Length + " bytes"); // Output: Compressed data is 2 bytes
 
-				// Decompress data
-				stream.Seek(0, SeekOrigin.Begin);
-				var decompressed = new UInt64[3];
-				codec.DecodeMany(stream, decompressed);
-				Console.WriteLine(String.Join(",", decompressed)); // Output: 1,2,3
+// Decompress data
+stream.Seek(0, SeekOrigin.Begin);
+var decompressed = new ulong[3];
+codec.DecodeMany(stream, decompressed);
+Console.WriteLine(string.Join(",", decompressed)); // Output: 1,2,3
 
-				Console.WriteLine("Done.");
-				Console.ReadKey(true);
-			}
-		}
-	}
-}
+Console.WriteLine("Done.");
+Console.ReadKey(true);
