17fedf9180bd73faa1c2c4d421ba51f0c3badc43
Ben Thompson <ben.thompson@lup.events>
2022-07-06 19:53:40 +1000

    Added additional TA tests and tidied file.

--- Test/Compression/Integers/ThompsonAlphaTests.cs
+++ Test/Compression/Integers/ThompsonAlphaTests.cs
@@ -6,6 +6,7 @@ namespace InvertedTomato.Compression.Integers
 {
     public class ThompsonAlphaTests
     {
+        // Encode
         private Byte[] Encode(UInt64 value)
         {
             var codec = new ThompsonAlphaCodec();
@@ -18,6 +19,29 @@ namespace InvertedTomato.Compression.Integers
             return stream.ToArray();
         }
 
+        [Fact]
+        public void Encode_0() => Assert.Equal(new Byte[] {0b000000_00}, Encode(0)); // Len=0, Val=(1)
+
+        [Fact]
+        public void Encode_1() => Assert.Equal(new Byte[] {0b000001_0_0}, Encode(1)); // Len=1, Val=(1)1
+
+        [Fact]
+        public void Encode_2() => Assert.Equal(new Byte[] {0b000001_1_0}, Encode(2)); // Len=10, val=(1)10
+
+        [Fact]
+        public void Encode_3() => Assert.Equal(new Byte[] {0b000010_00}, Encode(3)); // Len=10, val=(1)11
+
+        [Fact]
+        public void Encode_8589934590() => Assert.Equal(new Byte[] {0b100000_11, 0b11111111, 0b11111111, 0b11111111, 0b111111_00}, Encode(8589934590));
+
+        [Fact]
+        public void Encode_8589934591() => Assert.Equal(new Byte[] {0b100001_00, 0b00000000, 0b00000000, 0b00000000, 0b0000000_0}, Encode(8589934591));
+
+        [Fact]
+        public void Encode_Max() => Assert.Equal(new Byte[] {0b111111_11, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111000}, Encode(UInt64.MaxValue - 1));
+
+        // Decode
+
         private UInt64 Decode(Byte[] encoded)
         {
             var codec = new ThompsonAlphaCodec();
@@ -26,68 +50,27 @@ namespace InvertedTomato.Compression.Integers
 
             return codec.DecodeUInt64(reader);
         }
+        
+        [Fact]
+        public void Decode_0() => Assert.Equal((UInt64) 0, Decode(new Byte[] {0b000000_00}));
 
         [Fact]
-        public void Encode_0()
-        {
-            Assert.Equal(new Byte[] {0b000000_00}, Encode(0)); // Len=0, Val=(1)
-        }
+        public void Decode_1() => Assert.Equal((UInt64) 1, Decode(new Byte[] {0b000001_0_0})); // (len)_(val)_(padding)
 
         [Fact]
-        public void Encode_1()
-        {
-            Assert.Equal(new Byte[] {0b000001_0_0}, Encode(1)); // Len=1, Val=(1)1
-        }
+        public void Decode_2() => Assert.Equal((UInt64) 2, Decode(new Byte[] {0b000001_1_0}));
 
         [Fact]
-        public void Encode_2()
-        {
-            Assert.Equal(new Byte[] {0b000001_1_0}, Encode(2)); // Len=10, val=(1)10
-        }
+        public void Decode_3() =>  Assert.Equal((UInt64) 3, Decode(new Byte[] {0b000010_00}));
 
         [Fact]
-        public void Encode_3()
-        {
-            Assert.Equal(new Byte[] {0b000010_00}, Encode(3)); // Len=10, val=(1)11
-        }
+        public void Decode_8589934590() => Assert.Equal((UInt64) 8589934590, Decode(new Byte[] {0b100000_11, 0b11111111, 0b11111111, 0b11111111, 0b111111_00}));
+        
+        [Fact]
+        public void Decode_8589934591() => Assert.Equal((UInt64) 8589934591, Decode(new Byte[] {0b100001_00, 0b00000000, 0b00000000, 0b00000000, 0b0000000_0}));
 
         [Fact]
-        public void Encode_Max()
-        {
-            Assert.Equal(new Byte[] {0b111111_11, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111000}, Encode(UInt64.MaxValue - 1));
-        }
-
-
-        [Fact]
-        public void Decode_0()
-        {
-            Assert.Equal((UInt64) 0, Decode(new Byte[] {0b000000_00}));
-        }
-
-        [Fact]
-        public void Decode_1()
-        {
-            Assert.Equal((UInt64) 1, Decode(new Byte[] {0b000001_0_0})); // (len)_(val)_(padding)
-        }
-
-        [Fact]
-        public void Decode_2()
-        {
-            Assert.Equal((UInt64) 2, Decode(new Byte[] {0b000001_1_0}));
-        }
-
-        [Fact]
-        public void Decode_3()
-        {
-            Assert.Equal((UInt64) 3, Decode(new Byte[] {0b000010_00}));
-        }
-
-
-        [Fact]
-        public void Decode_Max()
-        {
-            Assert.Equal(UInt64.MaxValue - 1, Decode(new Byte[] {0b111111_11, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111000}));
-        }
+        public void Decode_Max() => Assert.Equal(UInt64.MaxValue - 1, Decode(new Byte[] {0b111111_11, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111000}));
 
         [Fact]
         public void EncodeDecode_1000()
