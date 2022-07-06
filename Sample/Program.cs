533293af9cf69154c5be062bfb472a23658d70f5
Ben Thompson <ben.thompson@lup.events>
2022-07-06 20:41:29 +1000

    Added comments to sample

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,31 +1,42 @@
 ﻿using InvertedTomato.Compression.Integers;
 
+// Instantiate the codecs we want
 var vlq = new VlqCodec();
+var fib = new FibonacciCodec();
 var td = new ThompsonAlphaCodec();
 
+// Encode some values...
 using var stream = new MemoryStream();
-
-// Encode
 using (var writer = new StreamBitWriter(stream))
 {
-    td.EncodeInt32(1, writer);
-    td.EncodeInt32(2, writer);
-    vlq.EncodeInt32(3, writer);
-    writer.Align();
-    td.EncodeInt32(4, writer);
+    // Encode some values using the Fibonacci codec
+    fib.EncodeInt32(0, writer);
+    fib.EncodeInt32(1, writer);
+    
+    // Encode a value using the VLQ codec
+    vlq.EncodeInt32(2, writer);
+    
+    // Encode a value using the ThompsonAlpha codec
+    td.EncodeInt32(3, writer);
 }
 
-Console.WriteLine("Compressed data is " + stream.Length + " bytes");
-stream.Seek(0, SeekOrigin.Begin);
+// Convert it to binary so we can see what it's done
+var binary = String.Join(" ", stream.ToArray().Select(a => Convert.ToString(a, 2).PadLeft(8, '0')));
+Console.WriteLine($"Compressed data is {stream.Length} bytes ({binary})");
 
-// Decode
+// Decode the values...
+stream.Seek(0, SeekOrigin.Begin);
 using (var reader = new StreamBitReader(stream))
 {
-    td.DecodeInt32(reader);
-    td.DecodeInt32(reader);
-    vlq.DecodeInt32(reader);
-    reader.Align();
-    td.DecodeInt32(reader);
+    // Decode the the Fibonacci values
+    Console.WriteLine(fib.DecodeInt32(reader));
+    Console.WriteLine(fib.DecodeInt32(reader));
+    
+    // Decode the VLQ value
+    Console.WriteLine(vlq.DecodeInt32(reader));
+    
+    // Decode the ThompsonAlpha value
+    Console.WriteLine(td.DecodeInt32(reader));
 }
 
 Console.WriteLine("Done.");
