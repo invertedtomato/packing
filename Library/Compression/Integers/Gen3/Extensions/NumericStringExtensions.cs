1ac4f8e719514fdae33d56879d0c7d89d64c0ad7
Ben Thompson <ben@thompsondigital.com.au>
2022-07-12 19:40:03 +1000

    Fixed bit padding

--- Library/Compression/Integers/Gen3/Extensions/NumericStringExtensions.cs
+++ Library/Compression/Integers/Gen3/Extensions/NumericStringExtensions.cs
@@ -6,7 +6,7 @@ namespace InvertedTomato.Compression.Integers.Gen3.Extensions;
 
 public static class NumericStringExtensions
 {
-    public static String ToBinaryString(this Byte[] target) => String.Join(" ", target.Select(a => Convert.ToString(a, 2).PadLeft(64, '0')));
+    public static String ToBinaryString(this Byte[] target) => String.Join(" ", target.Select(a => Convert.ToString(a, 2).PadLeft(8, '0')));
     public static String ToBinaryString(this UInt64 target) => Regex.Replace( Convert.ToString((Int64)target, 2).PadLeft(64, '0'),".{8}", "$0 ");
     
     public static String ToHexString(this Byte[] target) => BitConverter.ToString(target).Replace(" ","");
