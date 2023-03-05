7de01867089e944f3740e744fa2c3362c1ae96e4
Ben Thompson <ben@synth.earth>
2023-03-05 21:06:00 +1000

    Stripped unused code

--- Library/Integers/ZigZag.cs
+++ Library/Integers/ZigZag.cs
@@ -15,44 +15,10 @@ public static class ZigZag
     public static UInt64 Encode(Int64 value) => (UInt64)((value << 1) ^ (value >> 63));
 
     /// <summary>
-    /// Encode an array of signed longs into a ZigZag encoded array of unsigned longs
-    /// </summary>
-    /// <param name="values"></param>
-    /// <returns></returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static UInt64[] Encode(Int64[] values)
-    {
-        var output = new UInt64[values.Length];
-        for (var i = 0; i < values.Length; i++)
-        {
-            output[i] = Encode(values[i]);
-        }
-
-        return output;
-    }
-
-    /// <summary>
     /// Decode a ZigZag unsigned long back into a signed long
     /// </summary>
     /// <param name="value"></param>
     /// <returns></returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Int64 Decode(UInt64 value) => (Int64)((value >> 1) ^ (~(value & 1) + 1));
-
-    /// <summary>
-    /// Decode an array of unsigned longs into a ZigZag encoded array of signed longs
-    /// </summary>
-    /// <param name="values"></param>
-    /// <returns></returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Int64[] Decode(UInt64[] values)
-    {
-        var output = new Int64[values.Length];
-        for (var i = 0; i < values.Length; i++)
-        {
-            output[i] = Decode(values[i]);
-        }
-
-        return output;
-    }
 }
